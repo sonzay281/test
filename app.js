a659177a85c7650c613b78d26bd8cd8a124a394c
Sanjaya Sapkota <sonzayspk1998@gmail.com>
2025-06-10 00:06:40 -0500

    :bug: fix cors issue

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
 import {
   createStudent,
   deleteStudent,
@@ -7,7 +10,10 @@ import {
   updateStudent,
 } from "./services/studentService.js";
 
+dotenv.config();
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Read (all)
